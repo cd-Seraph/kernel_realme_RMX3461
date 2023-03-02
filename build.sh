c00fc0f5de7f4b8ce6e8925279feab24d6a3a44b
cd-Seraph <cloudchase04@gmail.com>
2023-03-02 09:01:08 +0000

    import: build script

---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Built in Timer
+SECONDS=0
+
+function compile() 
+{
+TZ='Asia/Manila'
+export TZ
+DATE=$(date "+%Y-%m-%d")
+rm -rf AnyKernel
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+export ARCH=arm64
+export KBUILD_BUILD_HOST=ice
+export KBUILD_BUILD_USER="cd-Seraph"
+ZIPNAME=Ice-Test-OSS-KERNEL-"${DATE}".zip
+kernel="out/arch/arm64/boot/Image"
+dtb="out/arch/arm64/boot/dts/vendor/oplus_7325/yupik.dtb"
+dtbo="out/arch/arm64/boot/dts/vendor/oplus_7325/yupik-21643-overlay.dtbo"
+
+
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+
+[ -d "out" ] && rm -rf out || mkdir -p out
+
+make O=out ARCH=arm64 feather_defconfig
+
+PATH="${PWD}/clang/bin:${PATH}" \
+make -j$(nproc --all) O=out \
+			ARCH=$ARCH \
+			CC=clang HOSTCC=clang \
+            CLANG_TRIPLE=aarch64-linux-gnu- \
+			CROSS_COMPILE=aarch64-linux-gnu- \
+			CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+            CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
+}
+
+function zupload()
+{
+if [ -f "$kernel" ] && [ -f "$dtb" ] && [ -f "$dtbo" ]; then
+	echo -e "\nKernel compiled succesfully! Zipping up...\n"
+
+      git clone --depth=1 https://github.com/cd-Seraph/AnyKernel3.git -b master AnyKernel
+	
+	cp $kernel AnyKernel
+	cp $dtb AnyKernel/dtb
+	python2 scripts/dtc/libfdt/mkdtboimg.py create AnyKernel/dtbo.img --page_size=4096 $dtbo
+	cd AnyKernel
+	zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+	cd ..
+	echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+	echo "Zip: $ZIPNAME"
+else
+	echo -e "\nCompilation failed!"
+	exit 1
+fi
+curl -sL https://git.io/file-transfer | sh
+./transfer wet *.zip
+}
+
+compile
+zupload
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
