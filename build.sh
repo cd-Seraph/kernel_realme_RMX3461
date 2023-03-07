b4ddee2ee01eafc02b50c34f3b35c3077ea36a34
cd-Seraph <120856294+cd-Seraph@users.noreply.github.com>
2023-03-07 09:43:52 +0800

    build script: do not run script as function
    also changed kbuild host

--- build.sh
+++ build.sh
@@ -2,17 +2,13 @@
 
 # Built in Timer
 SECONDS=0
-
-function compile() 
-{
 TZ='Asia/Manila'
 export TZ
 DATE=$(date "+%Y-%m-%d")
-rm -rf AnyKernel
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
 export ARCH=arm64
-export KBUILD_BUILD_HOST=ice
+export KBUILD_BUILD_HOST=feather
 export KBUILD_BUILD_USER="cd-Seraph"
 ZIPNAME=Feather-OSS-KERNEL-RELEASE-"${DATE}".zip
 
@@ -39,18 +35,13 @@ make -j$(nproc --all) O=out \
 			OBJCOPY=llvm-objcopy \
 			OBJDUMP=llvm-objdump \
 			STRIP=llvm-strip \
-            CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
-}
+            		CONFIG_NO_ERROR_ON_MISMATCH=y 2>&1 | tee error.log 
 
-function zupload()
-{
+
+
 git clone --depth=1 https://github.com/cd-Seraph/AnyKernel3.git -b master AnyKernel
 cp out/arch/arm64/boot/Image AnyKernel
 cd AnyKernel
 zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
 cd ..
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-}
-
-compile
-zupload
